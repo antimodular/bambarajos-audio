565d32afe5a4210bc0a7907257d3b6d44357e9ef
Glitch (hello-express) <none>
2020-05-06 15:06:52 +0000

    🔨🐬 Checkpoint ./audio.js:15722943/698

--- audio.js
+++ audio.js
@@ -4,7 +4,7 @@
 // frameRate() is usually around 60 frames per second,
 // so 20 fps = 3 beats per second, meaning if the song is over 180 BPM,
 // we wont respond to every beat.
-var beatHoldFrames = 30;
+var beatHoldFrames = 10; //30;
 
 // what amplitude level can trigger a beat?
 var beatThreshold = 0.25; //0.05; //0.11;
@@ -12,7 +12,7 @@ var beatThreshold = 0.25; //0.05; //0.11;
 // When we have a beat, beatCutoff will be reset to 1.1*beatThreshold, and then decay
 // Level must be greater than beatThreshold and beatCutoff before the next beat can trigger.
 var beatCutoff = 0;
-var beatDecayRate = 0.985; // how fast does beat cutoff decay?
+var beatDecayRate = 0.95; //0.985; // how fast does beat cutoff decay?
 var framesSinceLastBeat = 0; // once this equals beatHoldFrames, beatCutoff starts to decay.
 
 var beatRectSize = 100;
@@ -141,7 +141,7 @@ function spectrum(stream) {
   
       // audioLevel = audioLevel / 2;
       
-vol_smoothing = 0.0000001; //0.1; //999999;
+vol_smoothing = 0.95; //0.1; //999999;
       var  average = (vol_smoothing * old_audioLevel) + ((1 - vol_smoothing) * volNorm);
       
       // console.log("average "+average + " raw " + audioLevel + " old " + old_audioLevel);
@@ -194,7 +194,7 @@ var graph_scaler = 100;
       canvasCtx.stroke();
 
       var bogus = source; // avoid GC or the whole thing stops
-    }, (1000 * canvas.width) / audioCtx.sampleRate);
+    }, 30); //(1000 * canvas.width) / audioCtx.sampleRate);
   }
 }
 
