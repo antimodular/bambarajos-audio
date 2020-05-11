12da0e19293063ead1274b6eed7e801a36660746
Glitch (bambarajos-audio) <none>
2020-05-11 20:07:46 +0000

    💠🎽 Checkpoint ./audio.js:15722943/14 ./style.css:15722943/12

--- audio.js
+++ audio.js
@@ -117,7 +117,7 @@ var lastTouchMillis;
  var mapped_y = 0;
 var touchMoveY = 0;
 var slideAlpha = 1;
-var deviceOrientation = 0;
+// var deviceOrientation = 0;
 
 function spectrum(stream) {
   // var audioCtx = new AudioContext();
@@ -262,7 +262,7 @@ var isTouching = false;
       window.isTouching = false;
     }
 
-    deviceOrientation = window.orientation
+    // deviceOrientation = window.orientation
 
     // canvas.width = 400; //window.player.width; //window.innerWidth / 4 - 20;
     // canvas.height = 320; //window.player.height; //window.innerHeight / 4 - 20;
@@ -281,9 +281,9 @@ var isTouching = false;
 
     setInterval(() => {
       
-      if(deviceOrientation != 0){
+//       if(deviceOrientation != 0){
         
-      }
+//       }
       var graph_y = (canvas.height / 2); // * 3;
 
       if (window.canvas_mousePressed == true) {
